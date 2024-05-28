daef24430152f35ec0e9894508e6ec8d426c2e97
haruki7049 <tontonkirikiri@gmail.com>
2024-05-28 13:39:17 +0900

    chore: hoge

--- src/bin/exchan.rs
+++ src/bin/exchan.rs
@@ -1,4 +1,6 @@
-use axum::{routing::get, Router, extract::State};
+use axum::{extract::State, routing::get, Router};
+use serde::Serialize;
+use std::sync::Arc;
 use tokio::net::TcpListener;
 
 const ADDRESS: &str = "127.0.0.1:8888";
@@ -6,10 +8,10 @@ const ADDRESSES_PATH: &str = "/addresses";
 
 #[tokio::main]
 async fn main() {
-    let userlist: Vec<UserInfo> = vec![UserInfo {
+    let userlist: Arc<Vec<UserInfo>> = Arc::new(vec![UserInfo {
         username: "haruki7049".to_string(),
         phone_hash: vec!["test_hash".to_string()],
-    }];
+    }]);
 
     let app: Router = Router::new()
         .route(ADDRESSES_PATH, get(addresses))
@@ -22,7 +24,7 @@ async fn main() {
     axum::serve(listener, app).await.unwrap();
 }
 
-async fn addresses(State(userlist): State<Vec<UserInfo>>) -> String {
+async fn addresses(State(userlist): State<Arc<Vec<UserInfo>>>) -> String {
     serde_json::to_string(&userlist).expect("failed to serialize")
 }
 
@@ -30,7 +32,7 @@ async fn homepage() -> String {
     "there is no data... Please go to /addresses !!".to_string()
 }
 
-#[derive(Debug, Clone)]
+#[derive(Serialize, Debug, Clone)]
 struct UserInfo {
     username: String,
     phone_hash: Vec<String>,
