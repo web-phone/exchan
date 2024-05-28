1b5698c6b3df423dcbec2fd2d19b63d02f6c7122
haruki7049 <tontonkirikiri@gmail.com>
2024-05-28 11:23:15 +0900

    feat: Init basic webserver

--- src/bin/exchan.rs
+++ src/bin/exchan.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing::get, Router};
+use tokio::net::TcpListener;
+
+const ADDRESS: &str = "127.0.0.1:8888";
+const ADDRESSES_PATH: &str = "/addresses";
+
+#[tokio::main]
+async fn main() {
+    let app: Router = Router::new()
+        .route(ADDRESSES_PATH, get(addresses))
+        .route("/", get(homepage));
+
+    let listener: TcpListener = tokio::net::TcpListener::bind(ADDRESS).await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn addresses() -> String {
+    "No addresses".to_string()
+}
+
+async fn homepage() -> String {
+    "there is no data... Please go to /addresses !!".to_string()
 }
