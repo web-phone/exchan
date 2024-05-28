68cdf374673293613307e9f7dcba033d7c2d1aaa
haruki7049 <64677724+haruki7049@users.noreply.github.com>
2024-05-28 19:18:07 +0900

    feat: Create userlist (#3)
    * chore: Create UserInfo
    
    * chore: hoge
    
    * chore: hoge
    
    * feat: Init push_user
    
    * chore: nix fmt
    
    * chore: Edit exchan.rs
    
    * chore: Add Mutex
    
    * chore: Edit push_user

--- src/bin/exchan.rs
+++ src/bin/exchan.rs
@@ -1,14 +1,30 @@
-use axum::{routing::get, Router};
-use tokio::net::TcpListener;
+use axum::{
+    body::Body, extract::State, http::StatusCode, response::IntoResponse, response::Response,
+    routing::get, routing::post, Json, Router,
+};
+use serde::{Deserialize, Serialize};
+use std::sync::Arc;
+use std::sync::Mutex;
+use tokio::{
+    net::TcpListener,
+    //sync::Mutex
+};
 
 const ADDRESS: &str = "127.0.0.1:8888";
 const ADDRESSES_PATH: &str = "/addresses";
 
 #[tokio::main]
 async fn main() {
+    let userlist: Arc<Mutex<Vec<UserInfo>>> = Arc::new(Mutex::new(vec![UserInfo {
+        username: "haruki7049".to_string(),
+        phone_hash: vec!["test_hash".to_string()],
+    }]));
+
     let app: Router = Router::new()
         .route(ADDRESSES_PATH, get(addresses))
-        .route("/", get(homepage));
+        .route("/", get(homepage))
+        .route("/push_user", post(push_user))
+        .with_state(userlist);
 
     let listener: TcpListener = tokio::net::TcpListener::bind(ADDRESS).await.unwrap();
 
@@ -16,10 +32,32 @@ async fn main() {
     axum::serve(listener, app).await.unwrap();
 }
 
-async fn addresses() -> String {
-    "No addresses".to_string()
+async fn addresses(State(userlist): State<Arc<Mutex<Vec<UserInfo>>>>) -> impl IntoResponse {
+    serde_json::to_string(&userlist).expect("failed to serialize")
 }
 
-async fn homepage() -> String {
+async fn push_user(
+    State(userlist): State<Arc<Mutex<Vec<UserInfo>>>>,
+    Json(request): Json<Arc<UserInfo>>,
+) -> impl IntoResponse {
+    let mut userlist = userlist.lock().unwrap();
+    userlist.push(UserInfo {
+        username: request.username.clone(),
+        phone_hash: request.phone_hash.clone(),
+    });
+
+    Response::builder()
+        .status(StatusCode::CREATED)
+        .body(Body::from("User created successfully"))
+        .unwrap()
+}
+
+async fn homepage() -> impl IntoResponse {
     "there is no data... Please go to /addresses !!".to_string()
+}
+
+#[derive(Deserialize, Serialize, Debug, Clone)]
+struct UserInfo {
+    username: String,
+    phone_hash: Vec<String>,
 }
