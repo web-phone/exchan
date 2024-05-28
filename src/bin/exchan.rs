f2d5d16ff7fcf283ef3ed231a615264aa530d8c0
haruki7049 <tontonkirikiri@gmail.com>
2024-05-28 13:23:56 +0900

    chore: Create UserInfo

--- src/bin/exchan.rs
+++ src/bin/exchan.rs
@@ -6,6 +6,13 @@ const ADDRESSES_PATH: &str = "/addresses";
 
 #[tokio::main]
 async fn main() {
+    let userlist: Vec<UserInfo> = vec![
+        UserInfo {
+            username: "haruki7049".to_string(),
+            phone_hash: vec!["test_hash".to_string()],
+        },
+    ];
+
     let app: Router = Router::new()
         .route(ADDRESSES_PATH, get(addresses))
         .route("/", get(homepage));
@@ -22,4 +29,10 @@ async fn addresses() -> String {
 
 async fn homepage() -> String {
     "there is no data... Please go to /addresses !!".to_string()
+}
+
+#[derive(Debug)]
+struct UserInfo {
+    username: String,
+    phone_hash: Vec<String>,
 }
