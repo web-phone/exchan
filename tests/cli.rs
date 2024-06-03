6b764766f207ed4b345042d55ea6c75fbf3b33ed
haruki7049 <tontonkirikiri@gmail.com>
2024-06-04 05:49:33 +0900

    chore: Init test_debug_mode()

---
+++ tests/cli.rs
@@ -0,0 +1,14 @@
+use assert_cmd::cmd::Command;
+
+#[test]
+fn test_debug_mode() {
+    //let result = Command::cargo_bin("exchan")
+    //    .unwrap()
+    //    .arg("--debug")
+    //    .unwrap();
+
+    //assert_eq!(result.status.code(), Some(0));
+
+    // Incompleted test...
+    assert!(false);
+}
